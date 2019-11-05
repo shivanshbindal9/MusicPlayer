6482aa8a4dbe8488f791223f8f7a2e38541a1c58
shivanshbindal9 <shivanshbindal603@gmail.com>
2019-11-05 17:34:31 +0530

    Updates

---
+++ precache-manifest.9682bb1e5d09a413feb62bc49c2e00f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcdf75569ea3ab22d20106291e27f38",
+    "url": "/MusicPlayer/index.html"
+  },
+  {
+    "revision": "2e9269abb106f27b4fdb",
+    "url": "/MusicPlayer/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ebb3dd6d5279b5791654",
+    "url": "/MusicPlayer/static/js/2.97bfaf72.chunk.js"
+  },
+  {
+    "revision": "2e9269abb106f27b4fdb",
+    "url": "/MusicPlayer/static/js/main.a66c23e8.chunk.js"
+  },
+  {
+    "revision": "c921841d5dd918dfd733",
+    "url": "/MusicPlayer/static/js/runtime-main.6731608a.js"
+  },
+  {
+    "revision": "b9979651128d5857ef4c3b6f07370cbd",
+    "url": "/MusicPlayer/static/media/pixar.b9979651.jpg"
+  }
+]);
